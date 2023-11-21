05ce469bc30208ac173e9e60b8d031022ca71885
JohnatanSV <111749258+JohnatanSV@users.noreply.github.com>
2023-11-21 05:46:42 -0500

    Add files via upload
    proyecto SENA

---
+++ New_Gym/src/main/java/com/mycompany/new_gym/New_Gym.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.new_gym;
+
+/**
+ *
+ * @author Admin
+ */
+public class New_Gym {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
